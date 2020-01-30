31d0437564a1958a47149712463ebc7e9607e382
Charles Celerier <cceleri@cs.stanford.edu>
2020-01-30 17:39:01 +0000

    cystring: Rewrite compose_str to grow a buffer as needed to write a string
    The original implementation of compose_str used vsnprintf to measure the
    size of the formatted string prior to allocation. This can be slow since
    we will always have to "write the string twice". For small strings, a
    sufficiently large initial buffer size will allow us to get away with
    only writing once.

--- src/cystring/cystring.c
+++ src/cystring/cystring.c
@@ -1,18 +1,43 @@
 #include "cystring.h"
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define INITIAL_BUFFER_SIZE 128
+
 char *compose_str(const char *format, ...) {
-  va_list args, args_measure;
+  va_list args;
   va_start(args, format);
 
-  va_copy(args_measure, args);
-  int str_size = 1 + vsnprintf(NULL, 0, format, args_measure);
-  va_end(args_measure);
+  size_t buffer_size = INITIAL_BUFFER_SIZE;
+  char *str;
 
-  char *str = malloc(str_size);
-  vsnprintf(str, str_size, format, args);
+  while (true) {
+    str = malloc(buffer_size);
+    if (NULL == str) return NULL;
+    va_list args_loop;
+    va_copy(args_loop, args);
+
+    // try to write to str and break if successful
+    int num_bytes_written = vsnprintf(str, buffer_size, format, args_loop);
+    if (num_bytes_written < buffer_size) {
+      if (num_bytes_written < 0) {
+        return NULL;  // there must have been an encoding error
+      }
+      va_end(args_loop);
+      break;
+    }
+    va_end(args_loop);
+
+    // we need more buffer!
+    size_t prev_buffer_size = buffer_size;
+    buffer_size *= 2;
+    if (prev_buffer_size >= buffer_size) {
+      return NULL;  // the buffer size overflowed
+    }
+    free(str);
+  }
   va_end(args);
 
   return str;
